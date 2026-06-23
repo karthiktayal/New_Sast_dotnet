0b1621b6a1df459bd6c0c35b4a7573784b421bc1
karthik <karthiktayalan.madas@hcl-sofware.com>
2026-06-23 18:41:18 +0530

    Initial commit

---
+++ XtremelyEvilWebApp_core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XtremelyEvilWebApp_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
