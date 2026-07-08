68f373a1500739cb695c3b657c584f14ce6e66d6
karthiktayal <karthiktayalan.madas@hcl-software.com>
2026-07-08 10:38:47 +0530

    Rename Orderssse12345678re to Orderssse12345678reA360

--- Core_core/Order.cs
+++ Core_core/Order.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Core
 {
-    public class Orderssse12345678re
+    public class Orderssse12345678reA360
     {
         public virtual int OrderId { get; set; }
         public virtual string CustomerId { get; set; }
