30b4a9995ab42e4104a1b739cb5f168f8ec71c8e
karthiktayal <karthiktayalan.madas@hcl-software.com>
2026-07-08 14:48:42 +0530

    Rename Order class to Order A360_ASoC

--- Core_core/Order.cs
+++ Core_core/Order.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Core
 {
-    public class Order A360
+    public class Order A360_ASoC
     {
         public virtual int OrderId { get; set; }
         public virtual string CustomerId { get; set; }
