fd9e24a8b389c9a61f23ed02a249ec7ff83a716f
karthiktayal <karthiktayalan.madas@hcl-software.com>
2026-07-10 13:40:29 +0530

    Fix class declaration syntax in Order.cs

--- Core_core/Order.cs
+++ Core_core/Order.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Core
 {
-    public class Order A360inc1.1.0
+    public class Order A360inc1.1.0 sca scan
     {
         public virtual int OrderId { get; set; }
         public virtual string CustomerId { get; set; }
