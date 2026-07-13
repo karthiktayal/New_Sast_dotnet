fac534e4b6e369a3ac62f8ce56eaaf47ade730b7
karthiktayal <karthiktayalan.madas@hcl-software.com>
2026-07-13 10:52:22 +0530

    Update namespace and class name in CreditCard.cs

--- Core_core/CreditCard.cs
+++ Core_core/CreditCard.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 using System.Xml;
 using System.Xml.Linq;
 
-namespace Core 1.1.0 test config1
+namespace Core 1.1.0 test config12
 {
     public class CreditCard incremental scan core_core config12
     {
