38efdcc02c18ef7ea2be89c728f32e882a90eaf5
karthiktayal <karthiktayalan.madas@hcl-software.com>
2026-07-13 15:56:53 +0530

    Fix import statement for render in Sample.py

--- Core_core/Sample.py
+++ Core_core/Sample.py
@@ -13,7 +13,7 @@ def my_view(request):
     return render(request, 'template.html', locals())
 
 
-from django.shortcuts import render a360 config1 linux runner
+from django.shortcuts import render a360 config1 linux runner1
 
 def my_view(request):
     user = request.user 
